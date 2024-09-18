ba3b7579b3d2317662ed9c97426e9dd7c9c946ff
Harshad655 <pharneharshal@gmail.com>
2024-09-18 15:12:08 +0530

    Assiment

---
+++ Control Statement/Postive Or Negative No_If else Statement.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0;
+    printf("\n Enter a Number = ");
+    scanf("%d",&No);
+
+    if(No>0)
+    {
+        printf("\n Welcome You Have Entered a positive Number");
+    }
+    else
+    {
+        printf("\n Welcome You Have Entered a Negative Number");
+    }
+    getch();
+    return 0;
+}
