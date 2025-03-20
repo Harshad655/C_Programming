1d0762e4822bc9677a62bae60e8ebfc94bc964e1
Harshad Pharne <pharneharshal@gmail.com>
2025-03-20 13:16:23 +0530

    Add files via upload

---
+++ Assigement/Assigement No 2 Selection Statements/01. Program to check if a given number is even or odd.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num = 0;
+    printf("Enter An Number to check given number is Even or Odd :\n\n");
+    scanf("%d",&Num);
+
+    if(Num==0)
+    {
+        printf("Given Num is Zero");
+    }
+    else if(Num %2 == 0)
+    {
+        printf("Given no is Even");
+    }
+    else
+    {
+        printf("Given no is Odd");
+    }
+    getch();
+    return 0;
+}
