caf319088a88b4e29f0ba99e7c3ea79e395bd402
Harshad Pharne <pharneharshal@gmail.com>
2025-03-24 00:05:11 +0530

    Add files via upload

---
+++ Assignment/Assignment No - 01 (Variables, Data Types , InputOutput)/4.Program To Multiply Two Numbers Provided By The User.c
@@ -0,0 +1,18 @@
+//Write a program to multiply two numbers provided by the user and display the result
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int No1=0, No2=0;
+
+    printf("Enter 2 Numbers: \n");
+    scanf("%d%d",&No1,&No2);
+
+    printf("Multiplication of Given Numbers is %d * %d = %d",No1,No2,No1*No2);
+
+    getch();
+    return 0;
+}
+
