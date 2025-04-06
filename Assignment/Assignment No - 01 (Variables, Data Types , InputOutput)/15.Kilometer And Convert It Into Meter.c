651a3e57df938f2273eb99986a8b3c3c88367899
Harshad Pharne <pharneharshal@gmail.com>
2025-04-06 23:09:29 +0530

    Add files via upload

---
+++ Assignment/Assignment No - 01 (Variables, Data Types , InputOutput)/15.Kilometer And Convert It Into Meter.c
@@ -0,0 +1,17 @@
+//Write a program which accept distance in kilometer and convert it into meter.
+(1 kilometer = 1000 Meter)
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    float Km =0.0;
+    printf("Enter Distance in Kilometer: ");
+    scanf("%f",&Km);
+
+    printf("The distance of Kilometer in Meter is % Meter",km*1000);
+
+    getch();
+    return 0;
+}
